37eb07e9f53a3b68f2567aa4bc7db2a0165299a9
mahsa-arabi <mahsaarabi26@yahoo.com>
2021-01-30 01:08:46 +0330

    add class Node data members

--- Node.h
+++ Node.h
@@ -5,9 +5,20 @@
 #ifndef HUFFMAN_CODING_NODE_H
 #define HUFFMAN_CODING_NODE_H
 
-
+#include <iosfwd>
+#include <string>
+#include <vector>
+using namespace std;
 class Node {
-
+private:
+    Node* right;
+    Node* left;
+public:
+    bool isLeft;  //true if the node is left child of his parent
+    bool isRight; //true if the node is right child of his parent
+    Node* parent;
+    std::string data;
+    int numOfRepeats;
 };
 
 
